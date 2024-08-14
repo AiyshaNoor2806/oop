952dcc2fda9a1c2f94fc667a08482c44bb17924f
Aiysha Noor <161074894+AiyshaNoor2806@users.noreply.github.com>
2024-08-14 19:20:36 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+// import Choices from "inquirer/lib/objects/choices";
+class person {
+    _personality;
+    constructor() {
+        this._personality = 'Mystery';
+    }
+    userInput(input) {
+        if (input.includes('talk to other about yourself openly')) {
+            this._personality = 'happily';
+        }
+        else if (input.includes('keep your word to yourself')) {
+            this._personality = 'talk rude';
+        }
+    }
+    get personality() {
+        return this._personality;
+    }
+}
+class Main {
+    async main() {
+        const ans = await inquirer.prompt({
+            type: 'list',
+            name: 'choice',
+            message: chalk.yellow('tell your behaviour?'),
+            choices: [
+                '1: talk to others about my self.',
+                '2: keep quite and dont ask questions'
+            ]
+        });
+        let myPerson = new person();
+        myPerson.userInput(ans.choice);
+        console.log(`you are ${myPerson.personality}`);
+    }
+}
+const myObject = new Main();
+myObject.main();
---
+++ main.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer"
+import chalk from "chalk"
+// import Choices from "inquirer/lib/objects/choices";
+
+class person {
+    private _personality: string;
+    constructor() {
+        this._personality = 'Mystery';
+    }
+
+userInput(input:string) {
+if(input.includes ('talk to other about yourself openly')) {
+     this._personality = 'happily';
+} else if(input.includes('keep your word to yourself')) {
+    this._personality = 'talk rude';
+}
+}
+get personality() {
+    return this._personality
+}
+}
+
+class Main {
+    public async main() {
+        const ans = await inquirer.prompt({
+            type: 'list',
+            name: 'choice',
+            message: chalk.yellow ('tell your behaviour?'),
+            choices : [
+                '1: talk to others about my self.',
+                '2: keep quite and dont ask questions'
+            ]
+        })
+        
+        let myPerson = new person()
+
+        myPerson.userInput(ans.choice);
+        
+        console.log(`you are ${myPerson.personality}`);        
+    }
+}
+
+const myObject = new Main();
+myObject.main();
